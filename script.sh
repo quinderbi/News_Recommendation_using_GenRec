a90c2806d49e5f55d2bcfa784cf86a9c9334c70e
Quin Derbi <quinderbi@gmail.com>
2024-12-31 18:55:10 +0700

    update script

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
 echo "Test script"
+
+for model in DiffModel
+do
+    for fold in 1 
+    do
+        echo "Training $model fold $fold"
+        python train.py --model $model --fold $fold
+    done
+done
