bfd6eeb7c44cfb725f21c720c2c09f893c2c12e2
Quin Derbi <quinderbi@gmail.com>
2025-01-01 19:49:18 +0700

    yok

--- script.sh
+++ script.sh
@@ -1,18 +1,10 @@
 echo "Running script.sh"
 
-# param input == 1
-if [ "$#" -eq 1 ] 
-then
-    workers=$1
-else
-    workers= "default_worker"
-fi
-
 for model in FairGANModel DiffModel 
 do
     for fold in 1 
     do
         echo "Training $model fold $fold"
-        python train.py --model $model --fold $fold --workers $workers
+        python train.py --model $model --fold $fold
     done
 done
