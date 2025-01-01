28c0f0275ed6bd010c41bba1dcdef0f1d820f916
Quin Derbi <quinderbi@gmail.com>
2025-01-01 19:46:38 +0700

    cmon

--- train.py
+++ train.py
@@ -5,82 +5,7 @@ from Config import Config
 from Model import FairGANModel
 from Model import DiffModel
 import datetime
-
-# CONFIG = {
-#     "FairGANModel": {
-#         # "dataset": "ebnerd_demo",
-#         # "data_path": "ebnerd_demo/",
-#         'epochs': 50,
-#         'batch': 64,
-#         'ranker_gen_layers': [1000],
-#         'ranker_gen_activation': 'tanh',
-#         'ranker_gen_dropout': 0.0,
-#         'ranker_dis_layers': [1000],
-#         'ranker_dis_activation': 'tanh',
-#         'ranker_dis_dropout': 0.0,
-#         'controller_gen_layers': [1000],
-#         'controller_gen_activation': 'relu',
-#         'controller_gen_dropout': 0.0,
-#         'controller_dis_layers': [1000],
-#         'controller_dis_activation': 'relu',
-#         'controller_dis_dropout': 0.0,
-#         'ranker_gen_step': 2,
-#         'ranker_dis_step': 1,
-#         'controller_gen_step': 3,
-#         'controller_dis_step': 1,
-#         'controlling_fairness_step': 3,
-#         'ranker_gen_reg': 0.0001,
-#         'ranker_dis_reg': 0.0,
-#         'controller_gen_reg': 0.0,
-#         'controller_dis_reg': 0.0,
-#         'controlling_fairness_reg': 0.0,
-#         'alpha': 0.001,
-#         'lambda': 0.01,
-#         'ranker_gen_lr': 1e-5,
-#         'ranker_gen_beta1': 0.9,
-#         'ranker_dis_lr': 1e-5,
-#         'ranker_dis_beta1': 0.9,
-#         'controller_gen_lr': 0.001,
-#         'controller_gen_beta1': 0.9,
-#         'controller_dis_lr': 0.001,
-#         'controller_dis_beta1': 0.9,
-#         'controlling_fairness_lr': 1e-5,
-#         'controlling_fairness_beta1': 0.9,
-#         'ranker_initializer': 'glorot_normal',
-#         'controller_initializer': 'glorot_normal',
-#         'debug': False
-#     },
-
-#     "DiffModel": {
-#         # "data_path": "ebnerd_demo/",
-#         "lr": 0.01,
-#         "weight_decay": 0.0,
-#         "batch_size": 64,
-#         "epochs": 50,
-#         "tst_w_val": False,
-#         "cuda": False,
-#         # "save_path": './saved_models/',
-#         "log_name":'log',
-#         "round": 1,
-
-#         # param for model
-#         "time_type":'cat',
-#         "dims": [1000],
-#         "norm": False,
-#         "emb_size": 100,
-
-#         # param for diffusion
-#         "mean_type": 'x0', # MeanType for diffusion: x0, eps
-#         "steps": 100,
-#         "noise_schedule":'linear-var',
-#         "noise_scale": 0.1,
-#         "noise_min": 0.001,
-#         "noise_max": 0.02,
-#         "sampling_noise": False,
-#         "sampling_steps": 0,
-#         "reweight": True,
-#     }
-# }
+import os
 
 
 def main():
@@ -172,14 +97,15 @@ def main():
         "g@20":[result["ndcg"][2]],
     })
 
-    try:
-        df_worker = pd.read_csv(r'./result/{}.csv'.format(args.worker_name))
-        df_worker = pd.concat([df_worker, df_result])
-    except:
-        df_worker = df_result
+    if not os.path.exists('./result'):
+        os.makedirs('./result')
+    # check if file exist
+    if os.path.exists(f'./result/{args.worker_name}.csv'):
+        df_result.to_csv(f'./result/{args.worker_name}.csv', mode='a', header=False, index=False)
+    else:
+        df_result.to_csv(f'./result/{args.worker_name}.csv', mode='w', header=True, index=False)
 
-    df_worker.to_csv(r'./result/{}.csv'.format(args.worker_name), index=False)
-    print("Result saved.")
+    
 
 
 if __name__ == "__main__":
